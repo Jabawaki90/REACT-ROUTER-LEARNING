ea1331c46792d7519642325078791ae23c2f1048
Ashraf Azemi <ashraf.chzm@gmail.com>
2022-08-01 23:09:07 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Product from "./pages/Product";
+import "./index.css";
+import App from "./App";
+import MainHeader from "./components/MainHeader";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <MainHeader />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
+  </BrowserRouter>
+);
